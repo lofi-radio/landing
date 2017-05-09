52f1a637e6ae55ea940a3319f895134c45bb60e7
Jakeoid <jake.triumphant@gmail.com>
2017-05-09 12:54:28 +1000

    Updating URLs and Adding Features
    - Updated the urls to now follow lofi-radio.co
    - Split TITLE and ARTIST into their own elements.
    - Added a notifications system and added it to nav.
    - Added a skip till next song feature.
    - Added CMD + M to mute the player.
    - Added a check if nobody is currently listening.
    - Added some output into the console when updating.

--- js/radiocontrols.js
+++ js/radiocontrols.js
@@ -14,6 +14,8 @@
 	audioplayer.volume = 0;
 
 	var isPaused = false;
+	var isNotify = false;
+	var isSkipNext = false;
 	var volumeRange = document.getElementById("volumerange");
 	volumerange.value = "100"
 
@@ -28,6 +30,24 @@
 
 	/** Add a listener for everytime someone presses a key **/
 	window.addEventListener("keydown", checkKeyPressed, false);
+
+	/** Make sure we have permissions in order to send notifications to the user **/
+	function toggleNotify() {
+		if (!Notification) {
+			return;
+		}
+
+		if (Notification.permission !== "granted")
+			Notification.requestPermission();
+
+		if(isNotify == false){
+			isNotify = true;
+			document.getElementById("notifystatus").textContent = "On";
+		} else {
+			isNotify = false;
+			document.getElementById("notifystatus").textContent = "Off";
+		}
+	}
 
 	/** Function to toggle the fullscreen for our clients **/
 	function toggleFullScreen() {
@@ -47,6 +67,33 @@
 		}
 	}
 
+	/** Toggle whether we should mute till the next song **/
+	function mutetillnext() {
+		/** If we're skipping to the next **/
+		if(isSkipNext == true){
+			/** Make it so we're not **/
+			isSkipNext = false;
+			/** Check if we have paused junk **/
+			if(isPaused == true){
+				/** Pause **/
+				vidplay();
+			}
+			/** Variables **/
+			var audioplayer = document.getElementById("audioplayer");
+			var volumeRange = document.getElementById("volumerange");
+			/** Volume **/
+			audioplayer.volume = volumerange.value / 100;
+		/** If we're not skipping to the next **/
+		} else {
+			/** Make it so we're not **/
+			isSkipNext = true;
+			/** Variables **/
+			var audioplayer = document.getElementById("audioplayer");
+			/** Volume **/
+			audioplayer.volume = 0;
+		}
+	}
+
 	/** Check what key is actually being pressed down. **/
 	function checkKeyPressed(e) {
 		/** Check if the spacebar is pressed */
@@ -55,12 +102,24 @@
 		}
 
 		/** TODO -- MAKE A MODAL MENU **/
-		if ((e.which == 191 || e.keyCode == 191) && e.metaKey) {
+		if ((e.which == 191 || e.keyCode == 191) && (e.metaKey || e.ctrlKey)) {
 			// CODE GOES HERE
 		}
 
+		/** Check if CMD + M is pressed **/
+		if ((e.which == 77 || e.keyCode == 77) && (e.metaKey || e.ctrlKey)) { 
+			/** Dont minimise the screen when we press junk **/
+			e.preventDefault();
+			/** Variables **/
+			var audioplayer = document.getElementById("audioplayer");
+			var volumeRange = document.getElementById("volumerange");
+			/** Volume **/
+			volumeRange.value = 0;
+			audioplayer.volume = 0;
+		}
+
 		/** Check if CMD + F is pressed **/
-		if ((e.which == 70 || e.keyCode == 70) && e.metaKey) {
+		if ((e.which == 70 || e.keyCode == 70) && (e.metaKey || e.ctrlKey)) {
 			/** Dont open our search menu **/
 			e.preventDefault();
 			/** Toggle the fullscreen mode of the browser **/
@@ -79,6 +138,8 @@
 		if (isPaused == false){
 			/* Change our value */
 			isPaused = true;
+			/* Change our isSkipNext */
+			isSkipNext = false;
 			/* Play the video */
 			audioplayer.volume = volumerange.value / 100;
 
--- js/radioinfo.js
+++ js/radioinfo.js
@@ -7,59 +7,91 @@
 	@author: jakeoid;
 	@contributors: n/a;
 
-**/
+	**/
 
-function updateCounts() {
+	var lastsongtitle = ""
+	var lastsongartist = ""
 
-	/** Grab our statistics **/
-	$.getJSON("http://lofi.jakeoid.com/stats", function(result){
-		/** Log to our console the new information **/
-		console.log('[INFO] Song Name and Listeners Count being added.')
+	function updateCounts() {
 
-		/** Get our variables to return in our information **/
-		var songname = result['icestats']['source']['title'] + " - " + result['icestats']['source']['artist']
-		var viewercount = result['icestats']['source']['listeners']
+		/** Grab our statistics **/
+		$.getJSON("http://lofi-radio.co/stats", function(result){
+			/** Log to our console the new information **/
+			console.log('[INFO] Song Name and Listeners Count being added.')
 
-		/** ********************************* **/
+			/** Get our variables to return in our information **/
+			var songname = result['icestats']['source']['title'] + " - " + result['icestats']['source']['artist']
+			var songtitle = result['icestats']['source']['title']
+			var songartist = result['icestats']['source']['artist']
+			var viewercount = result['icestats']['source']['listeners']
 
-		/** CURRENT SONG **/
-		$('#track-title').text(songname); 
-		
-		/** PAGE TITLE **/
-		document.title = songname + " | " + "LoFi Radio";
+			/** ********************************* **/
 
-		/** LISTENER COUNT **/
-		/** See that we have more than no viewers **/
-		if(viewercount > 0) {
-			/** We only have one viewer, show the information as only a singular person **/
-			if(viewercount == 1) {
-				/** Update the text. **/
-				$('#listener-count').text(viewercount + " person is currently chilling out."); 
-			/** We have more than one viewer, show the information as multiple people **/
+			/** Check that our last song that we checked for isnt the same **/
+			if(lastsongtitle != songtitle && lastsongartist != songartist){
+				/** Update it to be the same. **/
+				lastsongartist = songartist;
+				lastsongtitle = songtitle;
+
+				/** Check if our user wants notifications **/
+				if(isNotify == true) {
+					/** Send our user a notification letting them know about the new song **/
+					var notification = new Notification('Lofi Radio', {
+						body: songtitle + " - " + songartist,
+					});
+				}
+
+				/** Check if our user wants to skip to the next song **/
+				if(isSkipNext == true) {
+					/** Reset the value to the original **/
+					isSkipNext = false;
+					/** Variables **/
+					var audioplayer = document.getElementById("audioplayer");
+					var volumeRange = document.getElementById("volumerange");
+					/** Set the volume to the users input **/
+					audioplayer.volume = volumerange.value / 100;
+				}
+			}
+
+			/** ********************************* **/
+
+			/** CURRENT SONG **/
+			$('#title').text(songtitle); 
+			$('#artist').text(songartist); 
+
+			/** PAGE TITLE **/
+			document.title = songname + " | " + "LoFi Radio";
+
+			/** LISTENER COUNT **/
+			/** See that we have more than no viewers **/
+			if(viewercount > 0) {
+				/** We only have one viewer, show the information as only a singular person **/
+				if(viewercount == 1) {
+					/** Update the text. **/
+					$('#listener-count').text(viewercount + " person is currently chilling out."); 
+					/** We have more than one viewer, show the information as multiple people **/
+				} else {
+					/** Update the text. **/
+					$('#listener-count').text(viewercount + " people are currently chilling out."); 
+				}
+				/** Else we can just show that nobody is currently listening **/
 			} else {
 				/** Update the text. **/
-				$('#listener-count').text(viewercount + " people are currently chilling out."); 
+				$('#listener-count').text("Nobody is currently chilling out."); 
 			}
-		/** Else we can just show that nobody is currently listening **/
-		} else {
-			/** Update the text. **/
-			$('#listener-count').text("Nobody is currently chilling out."); 
-		}
 
-		/** Log some output **/
-		console.log('[INFO] Succesfully updated all of the values.')
+			/** Log some output **/
+			console.log('[INFO] Succesfully updated all of the values.')
+		});
+
+	}
+
+	/** Document Ready we establish the loops to run. **/
+	$(document).ready(function(){
+
+		/** Make sure we timeout this every 2 seconds. **/
+		setTimeout(function(){updateCounts();}, 2000);
+		/** Set the interval to check update information every 10 seconds. **/
+		setInterval(function(){updateCounts();}, 10000);
+
 	});
-	
-}
-
-/** Document Ready we establish the loops to run. **/
-$(document).ready(function(){
-
-	/** Make sure we timeout this every 2 seconds. **/
-	setTimeout(function(){updateCounts();}, 2000);
-	/** Set the interval to check update information every 10 seconds. **/
-	setInterval(function(){updateCounts();}, 10000);
-
-});
-
-/** TODO: MAKE ALERTS ON NEW SONGS **/
